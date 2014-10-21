4a804bac5f9ca5a8a4ec1ec465485d505037c2e2
dragomanov <adragomanov@gmail.com>
2014-10-21 16:01:26 +0300

    Added solution file

---
+++ JobHunters/JobHunters.Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using JobHunters.Data.Migrations;
+using JobHunters.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHunters.Data
+{
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
