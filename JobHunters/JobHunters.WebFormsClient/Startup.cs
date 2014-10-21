3e96bb3eb112fcaaf29c08f553ac1031f443f43b
dragomanov <adragomanov@gmail.com>
2014-10-21 15:58:14 +0300

    Part done skeleton

---
+++ JobHunters/JobHunters.WebFormsClient/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JobHunters.WebFormsClient.Startup))]
+namespace JobHunters.WebFormsClient
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
